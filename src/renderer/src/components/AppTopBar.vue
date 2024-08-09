15cc87d521ece1c93a85647de73d2d6012b420f1
淄博齐盛高级中学 - 非官方 <qsgz2023ji5ban@edicdn.eu.org>
2024-08-09 09:52:16 +0800

    feat: 添加全屏/还原界面的切换按钮

--- src/renderer/src/components/AppTopBar.vue
+++ src/renderer/src/components/AppTopBar.vue
@@ -14,16 +14,30 @@
 
     <template #append>
       <div class="ga-4">
-        <v-btn @click="router.push('/mainWindow')">打开配置</v-btn>
+        <v-btn @click="toggleFullScreen">{{ isFullScreen ? '还原' : '全屏' }}</v-btn>
+        <!-- 方便触屏全屏 --> 
+        <v-btn @click="router.push('/mainWindow')">退出当前配置</v-btn>
+        <!--原来携程打开配置文件了，这分明是退出吗！ -->
       </div>
     </template>
   </v-app-bar>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const ipcHandleExit = () => window.electron.ipcRenderer.send('prog:exit');
 
 const router = useRouter();
+const isFullScreen = ref(false);
+
+const toggleFullScreen = () => {
+  if (!isFullScreen.value) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+  isFullScreen.value = !isFullScreen.value;
+};
 </script>
