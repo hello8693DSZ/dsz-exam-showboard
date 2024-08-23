c51a7854f34fd42d07edeb847079ee7360d4e12c
淄博齐盛高级中学 - 非官方 <qsgz2023ji5ban@edicdn.eu.org>
2024-08-23 10:18:21 +0800

    chore: 简单的颜色和动画更改

--- src/renderer/src/pages/mainWindow.vue
+++ src/renderer/src/pages/mainWindow.vue
@@ -2,14 +2,14 @@
   <v-container class="main-container" fill-height>
     <v-row justify="center" align="center">
       <v-col cols="12" md="4">
-        <v-card class="pa-4" outlined>
-          <v-btn block color="primary" dark @click="openDialog"> 打开配置 </v-btn>
+        <v-card class="pa-4 fade-in slide-in" outlined>
+          <v-btn block color="deep-purple accent-4" dark @click="openDialog">打开配置</v-btn>
           <p class="mt-2 text-center">打开 JSON 配置文件</p>
         </v-card>
       </v-col>
       <v-col cols="12" md="4" class="mt-4 mt-md-0">
-        <v-card class="pa-4" outlined>
-          <v-btn block color="success" dark @click="gotoInfoPage"> 直接进入看板 </v-btn>
+        <v-card class="pa-4 fade-in slide-in" outlined>
+          <v-btn block color="teal accent-4" dark @click="gotoInfoPage">直接进入看板</v-btn>
           <p class="mt-2 text-center">直接进入看板，将继续使用上次加载的配置</p>
         </v-card>
       </v-col>
@@ -50,11 +50,37 @@ window.electron.ipcRenderer.on('common:openFile', (event, message) => {
   width: 100%;
   max-width: 300px;
   margin: auto;
+  transition: transform 0.3s ease-in-out;
+}
+.v-card:hover {
+  transform: scale(1.05);
 }
 .v-btn {
   height: 48px;
 }
 .text-center {
   text-align: center;
+}
+.fade-in {
+  animation: fadeIn 1s ease-in-out;
+}
+.slide-in {
+  animation: slideIn 1s ease-in-out;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@keyframes slideIn {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
 }
 </style>
