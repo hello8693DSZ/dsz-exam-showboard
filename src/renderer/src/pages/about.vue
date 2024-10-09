567c3cd7b5037b241997714eb2d28d17ea1e3e44
MKStoler <MKStoler1024@proton.me>
2024-10-09 12:04:15 +0800

    chore: 简单的关于

---
+++ src/renderer/src/pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="main-container" fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="8">
+        <v-card class="pa-4" outlined>
+          <v-card-title class="text-h4">关于考试展板</v-card-title>
+          <v-card-text>
+            <p>
+              欢迎来到考试展板应用程序。这是一个用于展示考试信息的工具，帮助考生更好地了解考试安排和状态。
+            </p>
+            <p>本应用程序旨在为考生提供便捷的考试信息查看体验。</p>
+            <p>版本号： 1.1.0-Malkuth</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.main-container {
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+.v-card {
+  width: 100%;
+  max-width: 600px;
+  margin: auto;
+  transition: transform 0.3s ease-in-out;
+}
+.v-card:hover {
+  transform: scale(1.05);
+}
+.v-btn {
+  height: 48px;
+}
+</style>
--- src/renderer/src/stores/app.js
+++ src/renderer/src/stores/app.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia';
 export const useProfileStore = defineStore('app', {
   state: () => ({
     examName: '考试名称',
-    appHeader: 'DSZ考试看板',
+    appHeader: '考试看板',
     message: '考试提醒信息',
     examInfos: []
   }),
